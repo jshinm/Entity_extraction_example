2d21a1d6425bd767003fa1c395dcf96d70922073
Jong Shin <jshinm@gmail.com>
2025-02-10 07:52:41 -0500

    clean code
    Signed-off-by: Jong Shin <jshinm@gmail.com>

--- src/dashboard.py
+++ src/dashboard.py
@@ -17,18 +17,13 @@ def load_model():
 st.sidebar.title("Control Panel")
 st.sidebar.write("Upload your data file in the following component.")
 st.sidebar.markdown("---")
-# st.sidebar.write("Use the sidebar to navigate through the app.")
 with st.sidebar:
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         st.write("File uploaded successfully!")
 
-# option = st.sidebar.selectbox("Choose a page:", ["Home", "Upload Data", "View Results"])
-
-# st.column_config.Column(width=500)
 col1, col2 = st.columns(2)
 with col1:
-    # st.header("Column 1")
     with st.expander("Extracted Entities", expanded=True):
         with st.container():
             engine = load_model()
@@ -41,17 +36,3 @@ with col2:
     with st.expander("Graph Representation", expanded=True):
         with st.container():
             components.html(engine.graph_html, height=500, width=None, scrolling=False)
-
-
-# Display content based on the selected option
-# if option == "Home":
-#     st.write("Welcome to the Entity Extraction Dashboard!")
-# elif option == "Upload Data":
-#     st.write("Upload your data here.")
-#     uploaded_file = st.file_uploader("Choose a file")
-#     if uploaded_file is not None:
-#         st.write("File uploaded successfully!")
-# elif option == "View Results":
-#     st.write("View the results of the entity extraction here.")
-
-# Run the app with: streamlit run /home/jshinm/Desktop/workspace/Entity_extraction_example/src/dashboard.py
